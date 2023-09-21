121d0d02388849016de3f532d104feda4359b224
Linjie Ding <pyrocat101@users.noreply.github.com>
2023-09-20 22:16:13 -0400

    use clang 15.0.6

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 set -eux
 
-curl -L "https://github.com/llvm/llvm-project/releases/download/llvmorg-16.0.3/$NAME.tar.xz" -o src.tar.xz
+curl -L "https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.6/$NAME.tar.xz" -o src.tar.xz
 tar "$WILDCARDS_FLAG" -xvf src.tar.xz \
-  "$NAME/bin/clang-16" \
+  "$NAME/bin/clang-15" \
   "$NAME/bin/clang++" \
   "$NAME/bin/clang-cpp" \
   "$NAME/bin/lld" \
@@ -13,14 +13,14 @@ tar "$WILDCARDS_FLAG" -xvf src.tar.xz \
   "$NAME/bin/llvm-ar" \
   "$NAME/bin/llvm-as" \
   "$NAME/bin/llvm-nm" \
-  "$NAME/lib/clang/16/include/*.h" \
+  "$NAME/lib/clang/15.0.6/include/*.h" \
   "$NAME/include/c++/v1"
 
 cd "$NAME"
 
 pushd bin
   if [  -z "NO_STRIP" ]; then
-    strip -SXx ./clang-16
+    strip -SXx ./clang-15
     # clang++ and clang-cpp are symlinks
     strip -SXx ./lld
     # ld.lld is a symlink
@@ -32,7 +32,7 @@ pushd bin
     strip -SXx ./llvm-libtool-darwin
   fi;
 
-  mv clang-16 clang
+  mv clang-15 clang
 
   # Fake some tools we don't care about.
   touch llvm-profdata
